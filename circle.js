52935bceed4f660d10d38fe9991448aded736956
Yasin <yyumrutepe19@posta.pau.edu.tr>
2022-02-24 08:40:16 +0300

    Patika Modül Ödevi

---
+++ circle.js
@@ -0,0 +1,12 @@
+const pi = Math.PI
+function circleArea(r){
+    area=pi.r*r
+    console.log(area)
+}
+
+function circleCircumference(r){
+ circumference=2*pi*r
+ console.log(circumference)
+}
+module.exports=circleArea
+module.exports=circleCircumference
---
+++ index.js
@@ -0,0 +1,6 @@
+const circleCircumference = require("./circle");
+const circleArea=require("./circle")
+
+circleCircumference(5)
+circleArea(5)
+
